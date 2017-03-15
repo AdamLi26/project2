d464bd0560816bd72430086c9295b855f1b7bc6d
Tom <thomas.chang7@yahoo.com>
2017-03-15 16:38:46 -0700

    Add some code

--- src/myclient.cpp
+++ src/myclient.cpp
@@ -9,7 +9,14 @@
 #include <utility.cpp>   // for dieWithError()
 #include <RDTSegment.h>
 
+#include <time.h> //for timeval
+
 using namespace std;
+
+struct ClientPacketModule {
+  bool received_ = false;
+  RDTSegment segment;
+};
 
 int main(int argc, char *argv[]) 
 {
@@ -37,25 +44,52 @@ int main(int argc, char *argv[])
     memset(&synSeg, 0, sizeof(struct RDTSegment));
     setSyn(&synSeg.header);
     toNetwork(&synSeg.header);
+    
+    //Declare an fd_set that will be used for entire "connection"
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(sockfd, &readfds);
 
-	if (sendto(sockfd, &synSeg, sizeof(struct RDTSegment), 0, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) 
-		dieWithError("ERROR, fail to send");
-	
-	// Receive a response 
-	struct sockaddr_in fromAddr;
-	unsigned int fromSize = sizeof(fromAddr);
+    //Timeout information for use with select()
+    struct timeval tv_synSeg;
+    tv_synSeg.tv_sec = 0;
+    tv_synSeg.tv_usec = 0; //MODIFY THIS!
 
-	struct RDTSegment recvMsg;
-	memset(&synSeg, 0, sizeof(struct RDTSegment));
+    while(true) {
+      if (sendto(sockfd, &synSeg, sizeof(struct RDTSegment), 0, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) 
+	dieWithError("ERROR, fail to send");
+      
+      // Receive a response 
+      struct sockaddr_in fromAddr;
+      unsigned int fromSize = sizeof(fromAddr);
+      
+      struct RDTSegment recvMsg;
+      memset(&synSeg, 0, sizeof(struct RDTSegment));
+ 
 
-	if (recvfrom(sockfd, &recvMsg, sizeof(RDTSegment), 0, (struct sockaddr *) &fromAddr, &fromSize) < 0)
-		dieWithError("ERROR, fail to receive");
+      int synSeg_ret = select(sockfd + 1, &readfds, NULL, NULL, &tv_synSeg);
+      if(synSeg_ret > 0) {
+	//Received data
+	if(recvfrom(sockfd, &recvMsg, sizeof(RDTSegment), 0, (struct sockaddr*) &fromAddr, &fromSize) < 0) {
+	    dieWithError("ERROR, fail to receive");
+	}
+	if(serverAddress.sin_addr.s_addr != fromAddr.sin_addr.s_addr) {
+	  dieWithError("ERROR, unknown server");
+	}
+	//INCREMENT RECV_BASE
+	cout << "Receiving packet 0" << endl;
+	break;
+      }
+      else if(synSeg_ret == 0) {
+	//Timout occured 
+      }
+      else {
+	perror("Select error occured");
+      }
 
-	if (serverAddress.sin_addr.s_addr != fromAddr.sin_addr.s_addr) 
-		dieWithError("ERROR, unknown server");
-
-	cout << "Client Received: " << endl;
-	print(&recvMsg);
-	close(sockfd); 
-	return 0;
+    } //while loop
+    cout << "Client Received: " << endl;
+    print(&recvMsg);
+    close(sockfd); 
+    return 0;
 }
